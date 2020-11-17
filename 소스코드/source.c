39a6f71aba75a35448cb221e2c8926a740a0b8bd
Hyeon9mak <jinha3507@gmail.com>
2020-11-17 13:27:16 +0900

    Update source.c
    최성우씨가 리퀘스트 주신 코드 적용

--- 소스코드/source.c
+++ 소스코드/source.c
@@ -3,15 +3,16 @@
 #include<time.h>
 #include<windows.h> 
 
-#define EPSILON 0.1
-#define DC_RATE 0.5
+#define EPSILON 0.3
+#define DC_RATE 0.7
 #define size 10
 #define EPISODE 30000
 
-typedef struct {
+typedef struct cell{
 	double R;
 	int state;
 	double direction[4];
+	struct cell* connection;
 }cell;
 
 cell map[size][size];
@@ -19,6 +20,7 @@ cell* ptr = &map[0][0];
 
 void init();
 void print();
+void sync();
 
 void action();
 int value(double num[4]);
@@ -34,20 +36,28 @@ int main() {
 	print();
 	for (int i = 0; i < EPISODE; i++) {
 		while (current != &map[size - 1][size - 1]) {
-			srand(unsigned(time(NULL)));
+			srand((unsigned)time(NULL));
+			sync();
 			current->state = 0;
+
 			action();
+			if (ptr->connection != NULL)
+				ptr = ptr->connection;
 			current = ptr;
 			current->state = 1;
 
 			print();
+
+
 			printf("%lf %lf %lf %lf %d", current->direction[0], current->direction[1], current->direction[2], current->direction[3], i);
 
-			Sleep(10);
+			Sleep(5);
 		}
 		current->state = 0;
 		ptr = &map[0][0];
 		current = ptr;
+		current->state = 1;
+
 	}
 }
 
@@ -55,11 +65,11 @@ void init() {
 	for (int i = 0; i < size; i++) {
 		for (int j = 0; j < size; j++) {
 			map[i][j].R = 0;
+			map[i][j].connection = NULL;
 			if (!i && !j) {
-				map[i][j].state = 1;//0,0Àº ÇöÀç À§Ä¡·Î ¼³Á¤ 
+				map[i][j].state = 1;//0,0ì€ í˜„ìž¬ ìœ„ì¹˜ë¡œ ì„¤ì • 
 			}
 			else {
-				map[i][j].R = 0;
 				map[i][j].state = 0;
 			}
 		}
@@ -76,15 +86,19 @@ void init() {
 				map[i][j].direction[0] = -2;
 		}
 	}
-	/*¿©±âºÎÅÍ ¸Ê ÃÊ±âÈ­ ½ÃÀÛ*/
-	map[9][8].R = -3;
-	map[8][7].R = -3;
-	map[7][6].R = -3;
-	map[6][5].R = -3;
+	/*ì—¬ê¸°ë¶€í„° ë§µ ì´ˆê¸°í™” ì‹œìž‘*/
+	map[6][1].connection = &map[7][9];
+	map[5][3].connection = &map[0][6];
+	map[0][3].R = -3;
+	map[8][5].R = -3;
+	map[4][6].R = -3;
+	map[4][2].R = -3;
+	map[3][0].R = -3;
+	map[7][2].R = -3;
 	map[5][4].R = -3;
-	map[4][3].R = -3;
-	map[3][2].R = -3;
-	map[2][1].R = -3;
+	map[1][7].R = -3;
+	map[2][8].R = -3;
+	map[8][5].R = -3;
 	map[9][9].R = 3;
 
 }
@@ -92,40 +106,66 @@ void print() {
 	system("cls");
 	for (int i = 0; i < size; i++) {
 		for (int j = 0; j < size; j++) {
-			if (map[i][j].state == 1)
-				printf("¡Ü ");//ÇöÀç À§Ä¡
-			else if (map[i][j].R == -3)
-				printf("¡á ");//±úÁö±â ½¬¿î Áö¿ª
-			else if (map[i][j].R == 0)
-				printf("¡à ");//´Ü´ÜÇÑ Áö¿ª
-			else
-				printf("¡ã ");//¸ñÇ¥ÁöÁ¡
+			if ((i == 6 && j == 1) || (i == 5 && j == 3)) {
+				if (map[i][j].state == 1)
+					printf("â— ");//í˜„ìž¬ ìœ„ì¹˜
+				else
+					printf("â˜… ");
+			}
+			else if ((i == 7 && j == 9) || (i == 0 && j == 6)) {
+				if (map[i][j].state == 1)
+					printf("â— ");//í˜„ìž¬ ìœ„ì¹˜
+				else
+					printf("â˜† ");
+			}
+			else {
+				if (map[i][j].state == 1)
+					printf("â— ");//í˜„ìž¬ ìœ„ì¹˜
+				else if (map[i][j].R == -3)
+					printf("â–  ");//ê¹¨ì§€ê¸° ì‰¬ìš´ ì§€ì—­
+				else if (map[i][j].R == 0)
+					printf("â–¡ ");//ë‹¨ë‹¨í•œ ì§€ì—­
+				else if (map[i][j].R == 3)
+					printf("â–² ");//ëª©í‘œì§€ì 
+			}
 		}
 		printf("\n");
 	}
 }
 
-int rand_way(double num[4]) {// 0º¸´Ù Å©°Å³ª °°Àº Q°ªÀÇ ¹øÈ£¸¦ ¸®ÅÏ
+void sync() {
+	for (int i = 0; i < size; i++) {
+		for (int j = 0; j < size; j++) {
+			if (map[i][j].connection != NULL) {
+				for (int k = 0; k < 4; k++)
+					map[i][j].direction[k] = map[i][j].connection->direction[k];
+			}
+		}
+	}
+}
+
+int rand_way(double direction[4]) {// 0ë³´ë‹¤ í¬ê±°ë‚˜ ê°™ì€ Qê°’ì˜ ë²ˆí˜¸ë¥¼ ë¦¬í„´
 	int ret;
 
-	srand(unsigned(time(NULL)));
+	srand((unsigned)time(NULL));
 
-	for (;;)
-	{
+	while (1) {
 		ret = rand() % 4;
-		if (num[ret] >= -1)
+		if (direction[ret] >= 0)
 			return ret;
 	}
 }
 
 int value(double direction[4]) {
-
+	srand((unsigned)time(NULL));
 	int ret;
 	int cnt[4] = { 0, };
 
 	double max = direction[0];
 	for (int i = 0; i < 4; i++) {
-		if (max < direction[i]) {
+		if (max < direction[i] && direction[i] >= 0) {
+			for (int j = 0; j < i; j++)
+				cnt[j] = 0;
 			max = direction[i];
 			cnt[i] = 1;
 		}
@@ -147,28 +187,28 @@ void action() {
 		select = rand_way(ptr->direction);
 		//printf("%d\n",select);
 		switch (select) {
-		case 0://¿À¸¥ÂÊ
+		case 0://ì˜¤ë¥¸ìª½
 			if ((ptr + 1)->R != 0)
 				ptr->direction[0] = (ptr + 1)->R;
 			else
 				ptr->direction[0] = DC_RATE * (ptr + 1)->direction[value((ptr + 1)->direction)];
 			ptr = (ptr + 1);
 			break;
-		case 1://¿ÞÂÊ
+		case 1://ì™¼ìª½
 			if ((ptr - 1)->R != 0)
 				ptr->direction[1] = (ptr - 1)->R;
 			else
 				ptr->direction[1] = DC_RATE * (ptr - 1)->direction[value((ptr - 1)->direction)];
 			ptr = (ptr - 1);
 			break;
-		case 2://¾Æ·¡ÂÊ
+		case 2://ì•„ëž˜ìª½
 			if ((ptr + size)->R != 0)
 				ptr->direction[2] = (ptr + size)->R;
 			else
 				ptr->direction[2] = DC_RATE * (ptr + size)->direction[value((ptr + size)->direction)];
 			ptr = (ptr + size);
 			break;
-		case 3://À­ÂÊ
+		case 3://ìœ—ìª½
 			if ((ptr - size)->R != 0)
 				ptr->direction[3] = (ptr - size)->R;
 			else
@@ -182,28 +222,28 @@ void action() {
 		select = value(ptr->direction);
 		//printf("%d\n", select);
 		switch (select) {
-		case 0://¿À¸¥ÂÊ
+		case 0://ì˜¤ë¥¸ìª½
 			if ((ptr + 1)->R != 0)
 				ptr->direction[0] = (ptr + 1)->R;
 			else
 				ptr->direction[0] = DC_RATE * (ptr + 1)->direction[value((ptr + 1)->direction)];
 			ptr = (ptr + 1);
 			break;
-		case 1://¿ÞÂÊ
+		case 1://ì™¼ìª½
 			if ((ptr - 1)->R != 0)
 				ptr->direction[1] = (ptr - 1)->R;
 			else
 				ptr->direction[1] = DC_RATE * (ptr - 1)->direction[value((ptr - 1)->direction)];
 			ptr = (ptr - 1);
 			break;
-		case 2://¾Æ·¡ÂÊ
+		case 2://ì•„ëž˜ìª½
 			if ((ptr + size)->R != 0)
-				ptr->direction[2] = (ptr + 1 + size)->R;
+				ptr->direction[2] = (ptr + size)->R;
 			else
 				ptr->direction[2] = DC_RATE * (ptr + size)->direction[value((ptr + size)->direction)];
 			ptr = (ptr + size);
 			break;
-		case 3://À­ÂÊ
+		case 3://ìœ—ìª½
 			if ((ptr - size)->R != 0)
 				ptr->direction[3] = (ptr - size)->R;
 			else
